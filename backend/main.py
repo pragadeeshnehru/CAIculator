3c4d96fb84af76f838b256785225e91817ce215d
Pragadeesh Nehru <pragadeeshxenocrate@gmail.com>
2024-12-14 21:34:36 +0530

    _

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,11 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 from apps.calculator.route import router as calculator_router
-from constants import SERVER_URL, PORT, ENV
 from mangum import Mangum
 
+app = FastAPI()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
-
-app = FastAPI(lifespan=lifespan)
-
-
+# CORS Middleware (allow all origins for now)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
@@ -22,16 +14,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.get('/')
 async def root():
     return {"message": "Server is running"}
 
 app.include_router(calculator_router, prefix="/calculate", tags=["calculate"])
 
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host=SERVER_URL,
-                port=int(PORT), reload=(ENV == "dev"))
-
+# Convert FastAPI app to a serverless function using Mangum
 handler = Mangum(app)
